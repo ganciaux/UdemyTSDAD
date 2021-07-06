b00daf51c016a5135b23c0af2d8de1bb7a1811df
ganciaux <ghislain.anciaux@live.fr>
2021-07-06 07:12:47 +0200

    15. Utilizing Interfaces to Implement Drag & Drop

--- dist/app.js
+++ dist/app.js
@@ -113,16 +113,26 @@ class ProjectItem extends Component {
             return `${this.project.people} persons`;
         }
     }
-    configure() { }
-    ;
+    dragStartHandler(event) {
+        console.log(event);
+    }
+    dragEndHandler(_) {
+        console.log('DragEnd');
+    }
+    configure() {
+        this.element.addEventListener('dragstart', this.dragStartHandler);
+        this.element.addEventListener('dragend', this.dragEndHandler);
+    }
     renderContent() {
         this.element.querySelector('h2').textContent = this.project.title;
         this.element.querySelector('h3').textContent = this.persons + ' assigns';
         ;
         this.element.querySelector('p').textContent = this.project.description;
     }
-    ;
 }
+__decorate([
+    autobind
+], ProjectItem.prototype, "dragStartHandler", null);
 // ProjectList Class
 class ProjectList extends Component {
     constructor(type) {
