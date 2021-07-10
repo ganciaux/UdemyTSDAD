a0ad8635918b32c5901b2ec9acb9712295301d8a
ganciaux <ghislain.anciaux@live.fr>
2021-07-10 20:12:34 +0200

    11.5. Adding TypeScript Support with the ts-loader Package

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-import { ProjectList } from './components/project-list.js';
-import { ProjectInput } from './components/project-input.js';
-const prjInput = new ProjectInput();
-const activePrjList = new ProjectList('active');
-const finishedPrjList = new ProjectList('finished');
-//# sourceMappingURL=app.js.map
